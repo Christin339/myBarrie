c0c69844a6101c3a0334f24e80e452d36a50b107
ifotn <rich@infrontofthenet.com>
2022-09-27 09:34:54 -0400

    l4/2 - about & navbar

--- controllers/index.js
+++ controllers/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'My Barrie' });
 });
 
+/* GET /about */
+router.get('/about', (req, res) => {
+  // create a list of categories in memory
+  let categories = [{
+    'name': 'Eat/Drink'
+  },
+  {
+    'name': 'Shop'
+  },
+  {
+    'name': 'Play'
+  }]
+
+  // load the about view, pass a title value and the list of categories for display
+  res.render('about', { 
+    title: 'About My Barrie',
+    categories: categories
+  })
+})
+
 module.exports = router;
